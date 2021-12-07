20c2f1dbd46d10a887395ffc6360c4f90b65f2c9
glen-s-abraham <glenprojects666@gmail.com>
2021-12-07 12:45:55 +0530

    animations workout completed

--- Workout1/js/script.js
+++ Workout1/js/script.js
@@ -1,3 +1,17 @@
-$(()=>{
+$(function(){
+   $('.nav-bar').hide();
+   $('.nav-header').hide();
+   $('.nav-items').hide();
+   $('.landing').hide();
+   $('h1').hide();
+   $('p').hide();
+   $('button').hide();
+   $('.nav-bar').slideDown(1000);
+   $('.landing').delay(1000).slideDown(1000);
+   $('.nav-header').delay(1000).fadeIn(1000);
+   $('.nav-items').delay(1000).fadeIn(1000);
+   $('h1').delay(2000).fadeIn(2000);
+   $('p').delay(2000).fadeIn(2000);
+   $('button').delay(2000).fadeIn(2000);
    
 });
