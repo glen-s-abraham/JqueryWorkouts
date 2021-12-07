669d264d5b960a786a554d2fc50675c285a2a356
glen-s-abraham <glenprojects666@gmail.com>
2021-12-07 09:51:34 +0530

    fades

---
+++ Animations/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    // $('.red-box').fadeOut(2000);
+    // $('.green-box').fadeOut(2000);
+    // $('.green-box').fadeIn(2000);
+    // $('.green-box').fadeTo(2000, 0.5);
+    $('.red-box').fadeTo(3000, 0.2);
+    $('.green-box').fadeTo(2000, 0.5);
+    $('.blue-box').fadeTo(1000, 0.8);
+    $('.blue-box').fadeToggle(3000);
+    $('.blue-box').fadeToggle(3000);
+});
